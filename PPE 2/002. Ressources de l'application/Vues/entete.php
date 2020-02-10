6dc119ac3e4c82feaf101cf7ac638ad9c8320332
Spinogl <adrien.dollet@free.fr>
2020-02-10 14:43:38 +0100

    Add files via upload

---
+++ PPE 2/002. Ressources de l'application/Vues/entete.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>
+			Mon Agence 
+		</title>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<link rel="shortcut icon" href="avion.png" type="image/x-icon"/> 
+		<LINK rel=STYLESHEET href="styleVoyage.css" type="text/css">
+	<head>
+	<body>
+		<div class="fondMilieu">
+			<div class="cadreHautLogo">
+				<img src="Images/logo.jpg"></img>
+			</div>
+			<div class="cadreHautMilieu">
+				<br><h2><i>  " Pour que vos rêves deviennent réalité ... "</i></h2></br>
+			</div>
+			<div class = "cadreHautDroit">
+				<h2 class="apparaitreDrapeau"> Voy'in <h2>
+			</div><HTML>
---
+++ PPE 2/002. Ressources de l'application/Vues/menu.php
@@ -0,0 +1,47 @@
+<div class="menu">
+							<ul id="menu-bar">
+								<li><a href="#">GUIDES</a> 
+									<ul>
+										<li><A href = 'index.php?vue=guide&action=ajouter'>Ajouter un Guide</A></li>
+										<li><A href = 'index.php?vue=guide&action=visualiser'>Voir les Guides</A></li>
+										<li><a href="construction.php"><font color=#908E8E>Modifier un guide</font></a></li>
+									</ul>
+								</li>
+								<li><a href="#">PAYS</a>
+									<ul>
+										<li><A href = 'index.php?vue=pays&action=ajouter'>Ajouter un Pays</A></li>
+										<li><A href = 'index.php?vue=pays&action=visualiser'>Voir les Pays</A></li>
+										<li><a href="construction.php"><font color=#908E8E>Modifier un Pays</font></a></li>
+									</ul>
+								</li>
+
+								<li><a href="#">LANGUES</a>
+									<ul>
+										<li><a href="construction.php"><font color=#908E8E>Ajouter une langue</font></a></li>
+										<li><a href="construction.php"><font color=#908E8E>Voir une Langue</font></a></li>
+										<li><a href="construction.php"><font color=#908E8E>Modifier une Langue</font></a></li>
+									</ul>
+								</li>
+
+							    <li><a href="#">VOYAGES</a>
+									<ul>
+										<li><a href="construction.php"><font color=#908E8E>Ajouter un Voyage</font></a></li>
+										<li><a href="construction.php"><font color=#908E8E>Voir un Voyage</font></a></li>
+										<li><a href="construction.php"><font color=#908E8E>Modifier un Voyage</font></a></li>
+										<li><a href="construction.php"><font color=#908E8E>Voir les clients d'un Voyage</font></a></li>
+									</ul>
+							    </li>
+							</ul>
+							
+			</div>
+			<div class="infoSupplementaires">		
+				<div class="slideshow">
+					<p class="sContent">	
+						<img src="Images/fleuves.jpg"></img> 
+						<img src="Images/chateaux.jpg"></img>
+						<img src="Images/iles.jpg"></img>
+						<img src="Images/montagnes.jpg"></img>
+					</p>
+					
+				</div>
+			</div>
